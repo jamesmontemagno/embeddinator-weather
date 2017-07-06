a3ddd97b4e2958324f1514ab913d3fdbe5056ad5
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-07-06 16:50:34 -0500

    Android Studio project
    MainActivity.java has the good stuff

---
+++ MyWeatherAndroid/app/src/main/java/com/refractored/myweatherandroid/MainActivity.java
@@ -0,0 +1,51 @@
+package com.refractored.myweatherandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import weatherandroid_dll.*;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText city;
+    EditText state;
+    TextView weather;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button_getweather);
+        city = (EditText)findViewById(R.id.edittext_city);
+        state = (EditText)findViewById(R.id.edittext_state);
+        weather = (TextView)findViewById(R.id.textview_weather);
+        progressBar = (ProgressBar)findViewById(R.id.progressbar);
+
+        progressBar.setVisibility(View.INVISIBLE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XAMWeatherFetcher fetcher = new XAMWeatherFetcher(city.getText().toString(), state.getText().toString());
+                XAMWeatherResult result = fetcher.getWeather();
+                if (result != null) {
+                    weather.setText(result.getText());
+                } else {
+                    weather.setText("An error occurred :(");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Intent intent = new Intent(this, WeatherActivity.class);
+        //startActivity(intent);
+    }
+}
